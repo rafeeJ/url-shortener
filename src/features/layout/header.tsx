589f9e2094a1dea37f8259b73147b49edb375daa
Rafee Jenkins <rafeejenkins@gmail.com>
2025-03-24 17:37:33 +0000

    feat: init

---
+++ src/features/layout/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <div className='h-8 w-full bg-blue-200'>
+            <span>
+                URL Shortener
+            </span>
+        </div>
+    )
+}
