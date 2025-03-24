04b1aaa18787fe1d83d707a41d1b6c3e84e2811a
Rafee Jenkins <rafeejenkins@gmail.com>
2025-03-24 18:32:06 +0000

    feat: adds mw

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,11 +3,13 @@ import {
   pgTable,
   serial,
   timestamp,
+  uuid,
   varchar,
 } from "drizzle-orm/pg-core";
 
 export const urlsTable = pgTable("urls", {
   id: serial("id").primaryKey(),
+  userId: uuid("user_ud").notNull(),
   shortCode: varchar("short_code", { length: 10 }).notNull().unique(),
   originalUrl: varchar("original_url", { length: 2048 }).notNull(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const anonId = request.cookies.get("anon_id")?.value;
+
+  if (!anonId) {
+    const newId = uuidv4();
+    response.cookies.set("anon_id", newId, {
+      path: "/",
+      maxAge: 60 * 60 * 24 * 365,
+      sameSite: "lax",
+    });
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
