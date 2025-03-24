e564ca004844b4d49ad8855c7ea59fe1a562fbac
Rafee Jenkins <rafeejenkins@gmail.com>
2025-03-24 17:47:03 +0000

    form

--- src/features/url/url-form.tsx
+++ src/features/url/url-form.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type FormData = {
   url: string;
 };
 
+const schema = z.object({
+  url: z.string().url({ message: "Please enter a valid URL." }),
+});
+
 export const UrlForm = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FormData) => {
     console.log("Submitted URL:", data.url);
@@ -15,6 +25,7 @@ export const UrlForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Enter URL" {...register("url")} />
+      {errors.url && <p style={{ color: "red" }}>{errors.url.message}</p>}
       <button type="submit">Submit</button>
     </form>
   );
